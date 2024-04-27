07ebdf7235a3c72e01f9456fbaf43b03feffb365
Lucasdeo1 <lucasdeodatosilva@hotmail.com>
2024-04-27 10:44:03 -0300

    aprendendo sobre constante

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+
+
+        int numero = 1;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR = 3.14; //variavel em caixa alta seguida de do nome final se torna um constante,cujo seu valor não poderá ser alterado
+
+
     }
 }
