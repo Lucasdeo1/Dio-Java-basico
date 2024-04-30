8d8096b09e138f758ba3ecde3dfd8c36dc837e1b
LucasDeo1 <lucasdeodatosilva@hotmail.com>
2024-04-29 21:33:52 -0300

    usando objetos em um projetinho de TV

---
+++ App/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada?"+smartTv.ligada);
+        System.out.println("Canal atual?"+ smartTv.canal);
+        System.out.println("Volume atual "+smartTv.volume);
+
+    }
+}
